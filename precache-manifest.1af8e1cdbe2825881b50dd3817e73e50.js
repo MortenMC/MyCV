c878bb4347ee6ff6e80b1dbb1613c62e08136e18
Morten Mc <Morten.M.Cramer@infomedia.dk>
2020-02-28 14:47:33 +0100

    Updates

---
+++ precache-manifest.1af8e1cdbe2825881b50dd3817e73e50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160dbef0ca4b3e9471645b1c154301f1",
+    "url": "/MyCV.com/index.html"
+  },
+  {
+    "revision": "3c3a7c74ff7e2f82db17",
+    "url": "/MyCV.com/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "929fa6cdec7e08b679a2",
+    "url": "/MyCV.com/static/js/2.c8097cac.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/MyCV.com/static/js/2.c8097cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3a7c74ff7e2f82db17",
+    "url": "/MyCV.com/static/js/main.4cb82d2f.chunk.js"
+  },
+  {
+    "revision": "ae5c1b47793be9cdadb0",
+    "url": "/MyCV.com/static/js/runtime-main.7d19ee65.js"
+  },
+  {
+    "revision": "85a866721ce765bb8e7e014dad1cd5bb",
+    "url": "/MyCV.com/static/media/MMCLogoSort.85a86672.png"
+  },
+  {
+    "revision": "7a09cf82982f6f3ed75463bcb71569d1",
+    "url": "/MyCV.com/static/media/mig.7a09cf82.png"
+  }
+]);
